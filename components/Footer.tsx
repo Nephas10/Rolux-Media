b674f2fda89cad0272a5a797b34e468c7c03aafa
nephas <nephaskango@gmail.com>
2026-09-05 13:59:01 +0200

    added right media fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -79,7 +79,7 @@ export default function Footer(){
 
             <section>
                 <h4 className="mb-4 text-lg font-semibold">Social Links</h4>
-                <FaFacebook className=""/><Link href="https://web.facebook.com/search/top?q=rolux%20media">Rolux Media</Link>
+                <FaFacebook className=""/><Link href="https://web.facebook.com/RoluxMedia1">Rolux Media</Link>
 
                 <AiOutlineInstagram className=""/><Link href="https://www.instagram.com/roluxmediaofficial">Rolux Media Offical</Link>
             </section>
